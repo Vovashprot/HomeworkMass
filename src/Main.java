74078a44c097b636211b6a0eb2412f4b5708df4e
Vovashprot <vova.protasenko@gmail.com>
2023-03-05 18:21:47 +0300

    task1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+    public static void task1(){
+        int[] mass = new int[3];
+        mass[0] = 1;
+        mass[1] = 2;
+        mass[2] = 3;
+        for (int i=0; i<=2;i++){
+            System.out.println(mass[i]);
+        }
+
+    }
+}
