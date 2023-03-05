d7f4b6fde76eda0ec94b0bc367837c9fd3e8996b
Vovashprot <vova.protasenko@gmail.com>
2023-03-05 18:32:50 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,27 @@ public class Main {
         task1();
     }
     public static void task1(){
-        System.out.println("Task1");
+        System.out.println("Task1/2");
         int[] mass = new int[3];
         mass[0] = 1;
         mass[1] = 2;
         mass[2] = 3;
         double[] mass2 = {1.57,7.654,9.986};
         int[] mass3 = {22,44,22,5};
+        for (int i=0; i<=1;i++){
+            System.out.print(mass[i]+", ");
+        }
+        System.out.println(mass[2]);
+
+        for (int i=0; i<=1;i++){
+            System.out.print(mass2[i]+", ");
+        }
+        System.out.println(mass2[2]);
+
         for (int i=0; i<=2;i++){
-            System.out.println(mass[i]);
+            System.out.print(mass3[i]+", ");
         }
-        for (int i=0; i<=2;i++){
-            System.out.println(mass2[i]);
-        }
-        for (int i=0; i<=3;i++){
-            System.out.println(mass3[i]);
-        }
+        System.out.println(mass3[3]);
 
     }
 }
